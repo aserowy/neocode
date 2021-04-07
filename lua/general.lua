bb24c0a5fbaf055feebca7ca4710a509aed60c21
Alexander Serowy <serowy@hotmail.com>
2021-04-07 17:20:35 +0200

    added tab handling

--- lua/general.lua
+++ lua/general.lua
@@ -3,8 +3,8 @@ local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
 
 local function opt(scope, key, value)
     scopes[scope][key] = value
-    if scope ~= "o" then
-        scopes["o"][key] = value
+    if scope ~= 'o' then
+        scopes['o'][key] = value
     end
 end
 
@@ -14,5 +14,9 @@ vim.o.termguicolors = true
 
 vim.g.mapleader = ' '
 
-opt("w", "number", true)
-opt("w", "relativenumber", true)
+opt('w', 'number', true)
+opt('w', 'relativenumber', true)
+
+opt('b', 'expandtab', true)
+opt('b', 'shiftwidth', 4)
+opt('b', 'tabstop', 4)
