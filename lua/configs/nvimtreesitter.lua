7e7c1a11314f77011695a754b93e9664c6f537d4
Alexander Serowy <serowy@hotmail.com>
2021-04-08 06:10:20 +0200

    added indent and incremental_selection to trstr

--- lua/configs/nvimtreesitter.lua
+++ lua/configs/nvimtreesitter.lua
@@ -1,10 +1,22 @@
 local ts_config = require("nvim-treesitter.configs")
 
 ts_config.setup {
-	    ensure_installed = "maintained",
-	        highlight = {
-			        enable = true,
-				        use_languagetree = true
-					    }
-				    }
+    ensure_installed = "maintained",
+    highlight = {
+        enable = true,
+        use_languagetree = true
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
+    indent = {
+        enable = true
+    },
+}
 
