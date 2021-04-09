6d97dd4933fc02e4aca8781348305f8b9557e7a3
Alexander Serowy <serowy@hotmail.com>
2021-04-09 06:42:42 +0200

    added file tree rebindings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,8 +14,12 @@ a.nvim_set_keymap('n', '<Leader>fm', [[<Cmd>Neoformat<CR>]], opt)
 -- file tree
 keymap.file_tree = function(tree_cb)
     return {
-        ["l"] = tree_cb("edit"),
-        ["h"] = tree_cb("close_node"),
+        ['l'] = tree_cb('edit'),
+        ['h'] = tree_cb('close_node'),
+        ['<space>rn'] = tree_cb('full_rename'),
+        ['cc'] = tree_cb('cut'),
+        ['dd'] = tree_cb('remove'),
+        ['yy'] = tree_cb('copy'),
     }
 end
 
@@ -39,36 +43,36 @@ a.nvim_set_keymap('t', '<Leader>td', '<C-\\><C-N>:bd!<CR>', opt)
 -- navigation
 keymap.lsp_on_attach = function(bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opt)
-    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opt)
-    buf_set_keymap("n", "<space>h", "<Cmd>lua vim.lsp.buf.hover()<CR>", opt)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
-    buf_set_keymap("n", "<space>s", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opt)
-    buf_set_keymap("n", "<space>d", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opt)
-    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
-    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opt)
-    buf_set_keymap("n", "<space>dp", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opt)
-    buf_set_keymap("n", "<space>dn", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opt)
-    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opt)
+    buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opt)
+    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opt)
+    buf_set_keymap('n', '<space>hh', '<Cmd>lua vim.lsp.buf.hover()<CR>', opt)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opt)
+    buf_set_keymap('n', '<space>hs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opt)
+    buf_set_keymap('n', '<space>hd', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opt)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opt)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opt)
+    buf_set_keymap('n', '<space>dd', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opt)
+    buf_set_keymap('n', '<space>dp', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opt)
+    buf_set_keymap('n', '<space>dn', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opt)
+    buf_set_keymap('n', '<space>dl', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opt)
 end
 
 -- selection
 keymap.tree_sitter_incremental = function()
     return {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+        init_selection = 'gnn',
+        node_incremental = 'grn',
+        scope_incremental = 'grc',
+        node_decremental = 'grm',
     }
 end
 
 keymap.tree_sitter_textobjects = function()
     return {
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
     }
 end
 
