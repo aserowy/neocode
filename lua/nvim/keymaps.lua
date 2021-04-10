d63c65d22f42e9758a915ae44c653e81667685a2
Alexander Serowy <serowy@hotmail.com>
2021-04-10 10:48:54 +0200

    fixed indent

--- lua/nvim/keymaps.lua
+++ lua/nvim/keymaps.lua
@@ -5,17 +5,17 @@ local keymaps = {}
 keymaps.register = function (scope, mappings)
     local options = {noremap = true, silent = true}
 
-	for key, value in pairs(mappings) do
-		api.nvim_set_keymap(scope, key, value, options)
-	end
+    for key, value in pairs(mappings) do
+        api.nvim_set_keymap(scope, key, value, options)
+    end
 end
 
 keymaps.register_bufnr = function (bufnr, scope, mappings)
     local options = {noremap = true, silent = true}
 
-	for key, value in pairs(mappings) do
-		api.nvim_buf_set_keymap(bufnr, scope, key, value, options)
-	end
+    for key, value in pairs(mappings) do
+        api.nvim_buf_set_keymap(bufnr, scope, key, value, options)
+    end
 end
 
 return keymaps
