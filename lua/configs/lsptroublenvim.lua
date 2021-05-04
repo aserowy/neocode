431d319d7940d6d019137248fc630389b1f45759
Alexander Serowy <serowy@hotmail.com>
2021-05-04 11:13:39 +0200

    added autocmd to ensure close on no window

--- lua/configs/lsptroublenvim.lua
+++ lua/configs/lsptroublenvim.lua
@@ -1,4 +1,7 @@
+local vim = vim
 local mappings = require'mappings'
+
+vim.cmd [[autocmd WinEnter * if winnr('$') == 1 && &ft == 'LspTrouble' | q | endif]]
 
 require'trouble'.setup {
     action_keys = mappings.lsp_trouble
