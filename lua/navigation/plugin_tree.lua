665e37397a9e8261e08e371d17618f57d78a83df
aserowy <serowy@hotmail.com>
2021-07-06 12:19:34 +0200

    fixed nvimtree bindings
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/navigation/plugin_tree.lua
+++ lua/navigation/plugin_tree.lua
@@ -6,11 +6,11 @@ local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
 local mappings = {}
 for key, value in pairs(require("mappings").explorer) do
-	mappings[key] = tree_cb(value)
+	table.insert(mappings, { key = key, cb = tree_cb(value) })
 end
 
 for key, value in pairs(require("mappings").explorer_nocallback) do
-	mappings[key] = value
+	table.insert(mappings, { key = key, cb = value })
 end
 
 vim.g.nvim_tree_bindings = mappings
