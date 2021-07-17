7059862152ff13500d5b77dd9ed3558d060f1e90
aserowy <serowy@hotmail.com>
2021-07-17 13:05:40 +0200

    fmt
    Signed-off-by: aserowy <serowy@hotmail.com>

--- shell.nix
+++ shell.nix
@@ -2,10 +2,10 @@ with import <nixpkgs> { };
 mkShell rec {
   name = "neocode";
   buildInputs = [
-    stylua
-    sumneko-lua-language-server
     lua51Packages.luacheck
     nodePackages.prettier
+    stylua
+    sumneko-lua-language-server
   ];
   shellHook = ''
     # format and check -> fac :)
