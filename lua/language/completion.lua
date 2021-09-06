b820a20796f2f0f84e1e2bcdfd38e085529029fd
aserowy <serowy@hotmail.com>
2021-09-06 08:59:39 +0200

    fixed sidebar open on close for session
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/completion.lua
+++ lua/language/completion.lua
@@ -10,20 +10,6 @@ local function check_back_space()
 end
 
 local M = {}
---[[ function M.confirm()
-    if vim.fn.pumvisible() == 1 then
-        if vim.fn.complete_info()["selected"] ~= -1 then
-            return vim.fn["compe#confirm"](require("nvim-autopairs").esc("<cr>"))
-        elseif vim.fn.call("vsnip#available", { 1 }) == 1 then
-            return rtc("<plug>(vsnip-jump-next)")
-        else
-            return require("nvim-autopairs").autopairs_cr()
-        end
-    else
-        return require("nvim-autopairs").autopairs_cr()
-    end
-end ]]
-
 function M.jump_next(fallback)
     if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(rtc("<C-n>"), "n")
--- lua/startup/plugin_auto-session.lua
+++ lua/startup/plugin_auto-session.lua
@@ -1,7 +1,22 @@
 local M = {}
 M.setup = function()
+    vim.o.sessionoptions = "blank,buffers,curdir,folds,help,options,tabpages,winsize,resize,winpos,terminal"
+
+    local function pre_save()
+        if package.loaded["aerial"] then
+            vim.cmd("AerialClose")
+        end
+        if package.loaded["nvim-tree"] then
+            require("nvim-tree").close()
+        end
+        if package.loaded["trouble"] then
+            require("trouble").close()
+        end
+    end
+
     require("auto-session").setup({
         auto_save_enabled = true,
+        pre_save_cmds = { pre_save },
     })
 end
 
