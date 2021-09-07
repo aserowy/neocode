2064e302f1261dfe2dc457d33bce3b509dff177d
aserowy <serowy@hotmail.com>
2021-09-07 06:40:34 +0200

    added markdownlint as optional to null-ls
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/null-ls.lua
+++ lua/language/lsp/null-ls.lua
@@ -1,14 +1,22 @@
+local function generate_sources(null_ls)
+    local sources = {
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.rustfmt,
+        null_ls.builtins.formatting.stylua,
+    }
+
+    if require("language.lsp.which").path_exists("markdownlint") then
+        table.insert(sources, null_ls.builtins.diagnostics.markdownlint)
+    end
+
+    return sources
+end
+
 local M = {}
 function M.setup(_, on_attach)
     local null_ls = require("null-ls")
     null_ls.config({
-        sources = {
-            null_ls.builtins.diagnostics.markdownlint,
-
-            null_ls.builtins.formatting.prettier,
-            null_ls.builtins.formatting.rustfmt,
-            null_ls.builtins.formatting.stylua,
-        },
+        sources = generate_sources(null_ls),
     })
 
     require("lspconfig")["null-ls"].setup({
