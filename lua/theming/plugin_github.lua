d7d3c03584d4c003d9cf7864cfb80833eac19180
aserowy <serowy@hotmail.com>
2021-09-17 08:53:47 +0200

    fixed github style setter
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/theming/plugin_github.lua
+++ lua/theming/plugin_github.lua
@@ -1,12 +1,13 @@
 local M = {}
 M.setup = function()
     local config = require("theming.configuration").get({
-        style = "dark_default",
+        style = "dark",
         transparent = false,
     })
 
     require("github-theme").setup({
         sidebars = { "Aerial", "NvimTree", "qf", "vista_kind", "terminal", "packer" },
+        theme_style = config.style,
         transparent = config.transparent,
     })
 end
