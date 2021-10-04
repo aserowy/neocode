bb7a430e961f04bcbcd9677b1db0700928b7a43f
aserowy <serowy@hotmail.com>
2021-10-04 08:07:45 +0200

    added nixpkgs-fmt as formatter to null-ls
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/null-ls.lua
+++ lua/language/lsp/null-ls.lua
@@ -1,4 +1,7 @@
 local function generate_sources(null_ls)
+    local helpers = require("null-ls.helpers")
+    local methods = require("null-ls.methods")
+
     local sources = {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.rustfmt,
@@ -9,12 +12,25 @@ local function generate_sources(null_ls)
         table.insert(sources, null_ls.builtins.diagnostics.markdownlint)
     end
 
+    if require("language.lsp.which").path_exists("nixpkgs-fmt") then
+        table.insert(sources, helpers.make_builtin({
+            method = methods.internal.FORMATTING,
+            filetypes = { "nix" },
+            generator_opts = {
+                command = "nixpkgs-fmt",
+                to_stdin = true,
+            },
+            factory = helpers.formatter_factory,
+        }))
+    end
+
     return sources
 end
 
 local M = {}
 function M.setup(_, on_attach)
     local null_ls = require("null-ls")
+
     null_ls.config({
         sources = generate_sources(null_ls),
     })
