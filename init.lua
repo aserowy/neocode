baa68d493581519810a7389c67d3bfc76473be7f
aserowy <serowy@hotmail.com>
2022-01-03 16:41:14 +0100

    introduced second plugin manager for vscode (vim-plug)
    Signed-off-by: aserowy <serowy@hotmail.com>

--- init.lua
+++ init.lua
@@ -1,19 +1,30 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
-if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-    execute("packadd packer.nvim")
-end
-
 if not vim.g.vscode then
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+    if fn.empty(fn.glob(install_path)) > 0 then
+        execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+        execute("packadd packer.nvim")
+    end
+
     require("plugins")
     require("language").setup()
 
     require("settings").setup()
     require("mappings").setup()
 else
+    local install_path = fn.stdpath("data") .. "/plugged"
+
+    if fn.empty(fn.glob(install_path)) > 0 then
+        execute(
+            "!curl -fLo "
+                .. [[ "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]]
+                .. "--create-dirs "
+                .. "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+        )
+    end
+
     require("vscode").setup()
 end
--- lua/vscode.lua
+++ lua/vscode.lua
@@ -1,32 +1,15 @@
-local is_windows = jit.os == "Windows"
-
-local function join_paths(...)
-    local separator = "/"
-    if is_windows then
-        separator = "\\"
-    end
-    return table.concat({ ... }, separator)
-end
-
 local function set_plugins()
-    require("packer").startup({
-        function(use)
-            use("asvetliakov/vim-easymotion")
+    local use = vim.fn["plug#"]
 
-            use({
-                "b3nj5m1n/kommentary",
-                config = function()
-                    require("kommentary.config")
-                end,
-            })
+    vim.call("plug#begin")
 
-            use("unblevable/quick-scope")
-        end,
-        config = {
-            package_root = join_paths(vim.fn.stdpath("data"), "site", "vscode"),
-            compile_path = join_paths(vim.fn.stdpath("config"), "plugin", "vscode_compiled.lua"),
-        },
-    })
+    use("asvetliakov/vim-easymotion")
+    use("b3nj5m1n/kommentary")
+    use("unblevable/quick-scope")
+
+    vim.call("plug#end")
+
+    require("kommentary.config")
 end
 
 local function set_settings()
@@ -48,16 +31,6 @@ end
 
 local function set_mappings()
     local keymaps = require("nvim.keymaps")
-
-    keymaps.register("n", {
-        ["y"] = '"+y',
-        ["Y"] = '"+Y',
-    })
-
-    keymaps.register("v", {
-        ["y"] = '"+y',
-        ["Y"] = '"+Y',
-    })
 
     keymaps.register("n", {
         ["<C-b><C-s>"] = [[<cmd>call VSCodeNotify('workbench.action.files.save')<cr>]],
