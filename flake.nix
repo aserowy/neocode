b676c8bb5133213349f7eb3603238c65cac36247
aserowy <serowy@hotmail.com>
2022-01-11 07:48:40 +0100

    nixpkgs to unstable
    Signed-off-by: aserowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
