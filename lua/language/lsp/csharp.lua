ab27f08f549b0e51f14a5924b98bc2f7749966a1
aserowy <serowy@hotmail.com>
2022-04-07 10:21:26 +0200

    added cmd to omnisharp lsp
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/csharp.lua
+++ lua/language/lsp/csharp.lua
@@ -1,6 +1,9 @@
 local M = {}
 function M.setup(capabilities, on_attach)
+    local pid = vim.fn.getpid()
+
     require("lspconfig").omnisharp.setup({
+        cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(pid) },
         capabilities = capabilities,
         on_attach = on_attach,
     })
