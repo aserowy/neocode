743475e7a63e4da5f7e164884b6531cefaa1c2ae
aserowy <serowy@hotmail.com>
2023-04-09 19:12:31 +0200

    added dependencies to todo and trouble
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -4,18 +4,27 @@ return {
         config = function()
             require("navigation.plugin_tree")
         end,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
     },
     {
         "folke/trouble.nvim",
         config = function()
             require("navigation.plugin_trouble")
         end,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
     },
     {
         "folke/todo-comments.nvim",
         config = function()
             require("todo-comments").setup({})
         end,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
     },
     {
         "nvim-telescope/telescope.nvim",
