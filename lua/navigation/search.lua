c75e0426b94f4fa47b5213b48fd81d6c59130a1a
Serowy, Alexander (EXT/ADMIN) <Alexander.Serowy.ADMIN@union-investment.de>
2023-04-17 14:57:08 +0200

    telescope: git_search ignores git cwd for better search in mono repos
    Signed-off-by: Serowy, Alexander (EXT/ADMIN) <Alexander.Serowy.ADMIN@union-investment.de>

--- lua/navigation/search.lua
+++ lua/navigation/search.lua
@@ -1,6 +1,6 @@
 local function git_or_local()
     local opts = {}
-    local ok = pcall(require("telescope.builtin").git_files, opts)
+    local ok = pcall(require("telescope.builtin").git_files({ use_git_root = false }), opts)
     if not ok then
         require("telescope.builtin").find_files(opts)
     end
