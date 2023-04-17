7cf0e54b3bef19c482a637ec8f0e73308e5abe28
Serowy, Alexander (EXT/ADMIN) <Alexander.Serowy.ADMIN@union-investment.de>
2023-04-17 18:02:32 +0200

    dap: dotnet dll path fix
    Signed-off-by: Serowy, Alexander (EXT/ADMIN) <Alexander.Serowy.ADMIN@union-investment.de>

--- lua/language/dap/dotnet.lua
+++ lua/language/dap/dotnet.lua
@@ -1,15 +1,19 @@
 local dap = require("dap")
 
 function Dotnet_build_project()
-    local default_path = vim.fn.getcwd() .. "/"
-    if vim.g["dotnet_last_proj_path"] ~= nil then
-        default_path = vim.g["dotnet_last_proj_path"]
+    local request = function()
+        return vim.fn.input("Path to *proj", vim.fn.getcwd() .. "/", "file")
     end
 
-    local path = vim.fn.input("Path to your *proj file", default_path, "file")
-    vim.g["dotnet_last_proj_path"] = path
+    if vim.g["dotnet_last_proj_path"] == nil then
+        vim.g["dotnet_last_proj_path"] = request()
+    else
+        if vim.fn.confirm("Change project path? \n" .. vim.g["dotnet_last_proj_path"], "&yes\n&no", 2) == 1 then
+            vim.g["dotnet_last_proj_path"] = request()
+        end
+    end
 
-    local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
+    local cmd = "dotnet build -c Debug " .. vim.g["dotnet_last_proj_path"] .. " > /dev/null"
 
     print("")
     print("Cmd to execute: " .. cmd)
@@ -24,14 +28,13 @@ end
 
 function Dotnet_get_dll_path()
     local request = function()
-        return vim.fn.input("Path to dll", vim.g["dotnet_last_proj_path"] .. "/bin/Debug/", "file")
+        return vim.fn.input("Path to dll", vim.g["dotnet_last_proj_path"] .. "bin/Debug/", "file")
     end
 
     if vim.g["dotnet_last_dll_path"] == nil then
         vim.g["dotnet_last_dll_path"] = request()
     else
-        if vim.fn.confirm("Do you want to change the path to dll? \n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2)
-            == 1
+        if vim.fn.confirm("Change dll path for debugging? \n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2) == 1
         then
             vim.g["dotnet_last_dll_path"] = request()
         end
@@ -60,10 +63,7 @@ function M.setup()
             name = "launch netcoredbg",
             request = "launch",
             program = function()
-                if vim.fn.confirm("Should I recompile first?", "&yes\n&no", 2) == 1 then
-                    Dotnet_build_project()
-                end
-
+                Dotnet_build_project()
                 return Dotnet_get_dll_path()
             end,
         },
