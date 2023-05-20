e5ccd3604507a178da5fe9a27a06014992bc8120
aserowy <serowy@hotmail.com>
2023-05-20 15:42:42 +0200

    lspconfig: changed lsp for nix to nil_ls
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/nix.lua
+++ lua/language/lsp/nix.lua
@@ -1,6 +1,6 @@
 local M = {}
 function M.setup(capabilities, on_attach)
-    require("lspconfig").rnix.setup({
+    require("lspconfig").nil_ls.setup({
         capabilities = capabilities,
         on_attach = on_attach,
     })
