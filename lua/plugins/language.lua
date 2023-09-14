d1d128e5b2b4040ea24c922deebd31368477abf1
aserowy <serowy@hotmail.com>
2023-09-14 07:10:46 +0200

    cmp: removed cmd line completion (not working)
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/plugins/language.lua
+++ lua/plugins/language.lua
@@ -104,15 +104,6 @@ local function setup_cmp()
             { name = "buffer" },
         },
     })
-
-    cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-            { name = "path" },
-        }, {
-            { name = "cmdline" },
-        }),
-    })
 end
 
 local function setup_luasnip()
