08a4493c3b19e424a07c7cbec6c9ee2df3aeee81
aserowy <serowy@hotmail.com>
2023-10-15 14:30:22 +0200

    flake: changed flake utils with parts
    Signed-off-by: aserowy <serowy@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -14,33 +14,37 @@
 
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        overlayAttrs = {
-          inherit (config.packages) neocode;
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+        let
+          package = (with pkgs; (makeOverridable callPackage self { }));
+        in
+        {
+          overlayAttrs = {
+            inherit (config.packages) neocode;
+          };
+
+          packages.default = package;
+
+          devShells.default = with pkgs; mkShell {
+            buildInputs = [
+              gh
+              lua51Packages.luacheck
+              nil
+              nixpkgs-fmt
+              nodejs_20
+              nodePackages.markdownlint-cli
+              nodePackages.prettier
+              stylua
+              sumneko-lua-language-server
+            ];
+
+            shellHook = ''
+              # format and check -> fac :)
+              alias fac="prettier --write README.md \
+                  && stylua init.lua lua/ \
+                  && luacheck init.lua lua/"
+            '';
+          };
         };
-
-        packages.default = (with pkgs; (makeOverridable callPackage self { }));
-
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            gh
-            lua51Packages.luacheck
-            nil
-            nixpkgs-fmt
-            nodejs_20
-            nodePackages.markdownlint-cli
-            nodePackages.prettier
-            stylua
-            sumneko-lua-language-server
-          ];
-
-          shellHook = ''
-            # format and check -> fac :)
-            alias fac="prettier --write README.md \
-                && stylua init.lua lua/ \
-                && luacheck init.lua lua/"
-          '';
-        };
-      };
     };
 }
