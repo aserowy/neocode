586aad30060cf171e94456d7b528a88c2a1e0046
aserowy <serowy@hotmail.com>
2023-12-26 08:48:19 +0100

    lsp: added nu filetype
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/nu.lua
+++ lua/language/lsp/nu.lua
@@ -1,5 +1,7 @@
 local M = {}
 function M.setup(capabilities, on_attach)
+    vim.filetype.add({ extension = { nu = "nu" } })
+
     require("lspconfig").nushell.setup({
         capabilities = capabilities,
         on_attach = on_attach,
