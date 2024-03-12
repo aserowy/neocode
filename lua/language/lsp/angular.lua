c136f14b0a0bbad44a706743fca38b557aac108f
aserowy <serowy@hotmail.com>
2024-03-12 11:01:25 +0100

    lspconfig: extended root dir resolve for angularls
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/angular.lua
+++ lua/language/lsp/angular.lua
@@ -1,8 +1,11 @@
 local M = {}
+
+local nvm_lsp = require("lspconfig")
 function M.setup(capabilities, on_attach)
-    require("lspconfig").angularls.setup({
+    nvm_lsp.angularls.setup({
         capabilities = capabilities,
         on_attach = on_attach,
+        root_dir = nvm_lsp.util.root_pattern("angular.json", "nx.json", "package.json"),
     })
 end
 
