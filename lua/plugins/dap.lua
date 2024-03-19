cfd9019dc6a43d47a09763c29dec78a170f113bd
aserowy <serowy@hotmail.com>
2024-03-19 07:36:08 +0100

    lazy: updated plugins
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,7 +4,7 @@ return {
         config = function()
             require("language.dap.init").setup()
         end,
-        dependencies = { "rcarriga/nvim-dap-ui" },
+        dependencies = { "nvim-neotest/nvim-nio", "rcarriga/nvim-dap-ui" },
     },
     {
         "theHamsta/nvim-dap-virtual-text",
