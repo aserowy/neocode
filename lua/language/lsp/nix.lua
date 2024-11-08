0e23878aee06e7ca3f3ed9520080c2de9132b31a
aserowy <serowy@hotmail.com>
2024-11-08 06:56:53 +0100

    nix lsp: change lspconfig from nil_ls to nixd for a more complete lsp for nix
    Signed-off-by: aserowy <serowy@hotmail.com>

--- lua/language/lsp/nix.lua
+++ lua/language/lsp/nix.lua
@@ -1,6 +1,6 @@
 local M = {}
 function M.setup(capabilities, on_attach)
-    require("lspconfig").nil_ls.setup({
+    require("lspconfig").nixd.setup({
         capabilities = capabilities,
         on_attach = on_attach,
     })
